0f1e3578489f07c95444659a1a43cb65624b19d5
hyoseong3291 <wjdgytjd3291@gmail.com>
2019-05-08 16:19:40 +0900

    TTT 완료

--- UniversityRegisterCourses/src/control/TraineeDAO.java
+++ UniversityRegisterCourses/src/control/TraineeDAO.java
@@ -1,5 +1,24 @@
 package control;
 
+import java.util.ArrayList;
+
+import model.TraineeVO;
+
 public class TraineeDAO {
+
+	public ArrayList<String> getTraineeColumnName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<TraineeVO> getTraineeTotalList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<TraineeVO> getTraineeSubjectSearchList(String searchName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
